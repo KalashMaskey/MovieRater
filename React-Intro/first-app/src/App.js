6795dbf7787f7f2363c643371d30549f8e05a20f
kalash <kalashm24@gmail.com>
2020-06-04 15:20:13 +0545

    Styled-Components

--- React-Intro/first-app/src/App.js
+++ React-Intro/first-app/src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HeaderEg from './components/headerEg'
-import FooterEg from './components/footerEg'
+import HeaderEg from './components/headerEg';
+import FooterEg from './components/footerEg';
+import styled from 'styled-components';
 
 function createAlert(){
   alert('Hello');
 }
 
+const pStyle = {
+  fontSize: '2em',
+  color: 'white',
+}
+
+const Paragraph = styled.p`
+  font-size: 3em;
+  color: green;`;
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <p>
           <HeaderEg info="Is generated with Props"
             message="Props Example"/>
         </p>
-        <p>Some Content </p>
+        <p style={pStyle}>Some Content </p>
+        <Paragraph> NEW </Paragraph>
         <FooterEg message="Class Props Example" myalert={createAlert}/>
-      </header>
     </div>
   );
 }
