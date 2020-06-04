0732e28f5a767b05c255e318b23676914e3a2a0e
kalash <kalashm24@gmail.com>
2020-06-04 17:00:23 +0545

    Movie-Rater-Web-App

--- React-Intro/first-app/src/components/numbers.js
+++ React-Intro/first-app/src/components/numbers.js
@@ -1,16 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Numbers = () => {
-  const [numbers, setNumbers] = useState(['one','two','three'])
+  const [numbers, setNumbers] = useState(['one','two','three']);
+  const [letters, setLetters] = useState(['a','b','c']);
 
   const addNumber = () => {
     setNumbers([...numbers,'four'])
-
   }
+
+  const addLetter = () => {
+    setLetters([...letters,'d'])
+  }
+
+  useEffect(()=> {
+    console.log('Effects Triggred');
+  })
   return(
     <div>
     <h1>Numbers</h1>
-
+  
     { numbers.map( num => {
       return <h4>{num}</h4>
     })}
